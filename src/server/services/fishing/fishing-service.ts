023cc70605f19123ff5b5935ff0efbb0a300254c
Lil Baby <jermfromamerica@gmail.com>
2024-12-13 14:41:29 +0700

    player-entity

--- src/server/services/fishing/fishing-service.ts
+++ src/server/services/fishing/fishing-service.ts
@@ -1,14 +1,56 @@
 import { Service, OnStart, OnInit } from "@flamework/core";
+import PlayerEntity from "../player/player-entity";
+import { Conditioner } from "shared/utils/conditioner";
+import { Timer } from "@rbxts/timer";
+import { Events } from "server/network";
 
 @Service({})
 export class FishingService implements OnStart, OnInit {
-    onStart() {
-        
-    }
+	/**
+	 * A private validator to manages given parameteres in cases when they are null.
+     * @private
+	 */
+	private readonly validator: Conditioner.ConditionValidator;
 
-    onInit(): void | Promise<void> {
-        
-    }
+	/**
+	 * Maximum fishiog time per interval
+     * @private
+	 */
+	private readonly fishingInterval: number;
 
-    public giveRod(playerEntity) {}
+	/**
+	 * Timer for managing fishing interval
+     * @private
+	 */
+	private readonly fishingIntervalTimer: Timer;
+
+	/** @ignore */
+	constructor() {
+		this.fishingInterval = 5;
+		this.fishingIntervalTimer = new Timer(this.fishingInterval);
+
+		this.validator = new Conditioner.ConditionValidator();
+	}
+
+	onStart() {}
+	onInit(): void | Promise<void> {}
+
+	private getFishByChance() {}
+
+	public grantFishingRodToPlayer(playerEntity: PlayerEntity): void {
+		const player = this.validator.checkForNullable("Failed to get player", playerEntity.getEntity());
+        
+	}
+
+	public getFishingIntervalPercentage(timer: Timer): number {
+		return (this.getCurrentFishingIntervalTime(timer) / this.fishingInterval) * 100;
+	}
+
+	public getCurrentFishingIntervalTime(timer: Timer): number {
+		return timer.getTimeLeft();
+	}
+
+	public getFishingIntervalTime(): number {
+		return this.fishingInterval;
+	}
 }
---
+++ src/server/services/player/player-service.ts
@@ -0,0 +1,12 @@
+import { Service, OnStart, OnInit } from "@flamework/core";
+
+@Service({})
+export class PlayerService implements OnStart, OnInit {
+    onStart() {
+        
+    }
+
+    onInit(): void | Promise<void> {
+        
+    }
+}
